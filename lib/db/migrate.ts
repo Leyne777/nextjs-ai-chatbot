3c6ae6df6ab4b7a3221438e5ca94b88772ad72f8
Leyne777 <erdemgurguc@hotmail.com>
2025-07-12 14:11:18 +0400

    Update migrate.ts

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -1,8 +1,10 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
+
 
 config({
   path: '.env.local',
